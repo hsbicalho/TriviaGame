664afa75ca256a62f46e5951b0b47d08780f53b6
Mooteus <moteus10@gmail.com>
2022-05-25 16:50:44 -0300

    Fix/Test: Fix Login tests and Add configuration button test

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,45 +1,57 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Test Login', () => {
-
-  beforeEach(() => {
-    const { history } = renderWithRouterAndRedux(<App />);
-  });
-
   test('01- Checks login screen inputs', () => {
+    renderWithRouterAndRedux(<App />);
     const inputName = screen.getByTestId('input-player-name');
     const inputEmail = screen.getByTestId('input-gravatar-email');
     
     userEvent.type(inputName, 'Roberto');
     userEvent.type(inputEmail, 'roberto@gmail.com');
 
-    expect(inputName).toBe('Roberto');
-    expect(inputEmail).toBe('roberto@gmail.com');
+    expect(inputName.value).toBe('Roberto');
+    expect(inputEmail.value).toBe('roberto@gmail.com');
   });
 
   test('02- Checks if the button is disabled', () => {
+    renderWithRouterAndRedux(<App />);
     const loginButton = screen.getByTestId('btn-play');
     expect(loginButton).toBeDisabled();
   });
 
   test('03- Checks button functionality', () => {
-    
+    const { history } = renderWithRouterAndRedux(<App />);
+    const { pathname } = history.location;
+
     const loginButton = screen.getByTestId('btn-play');
 
     const inputName = screen.getByTestId('input-player-name');
     const inputEmail = screen.getByTestId('input-gravatar-email');
 
-    expect(inputName).toBe('Roberto');
-    expect(inputEmail).toBe('roberto@gmail.com');
+    userEvent.type(inputName, 'Roberto');
+    userEvent.type(inputEmail, 'roberto@gmail.com');
+
+    expect(inputName.value).toBe('Roberto');
+    expect(inputEmail.value).toBe('roberto@gmail.com');
 
     expect(loginButton).not.toBeDisabled();
 
     userEvent.click(loginButton);
-    //conferir se não precisar ser async depois
-    expect(history.location.pathname).toBe('/jogo');
+    expect(pathname).toBe('/jogo');
+  });
+
+  test('04- Checks the Configuration Button', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const { pathname } = history.location;
+
+    const configButton = screen.getByTestId('btn-settings');
+    expect(configButton).toBeInTheDocument();
+
+    userEvent.click(configButton);
+    expect(pathname).toBe('/configuracao');
   });
 });
