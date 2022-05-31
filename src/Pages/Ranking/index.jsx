2c0627394017106d661cd724fb8cfa2e9f802579
Pedro Mello <pedro.mello.rj@gmail.com>
2022-05-31 06:18:27 -0300

    Requisito 18 - Feito (Bonus)

--- src/Pages/Ranking/index.jsx
+++ src/Pages/Ranking/index.jsx
@@ -1,11 +1,17 @@
 // import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button data-testid="btn-go-home" type="button">Início</button>
+        </Link>
+      </>
     );
   }
 }
